775b5a773738fd72cd9175fd3d80b9f89fd9ae52
brandonamos <bpamos21@gmail.com>
2023-01-03 15:16:09 -0700

    updated

--- variables.tf
+++ variables.tf
@@ -1,39 +1,52 @@
-variable "rediscloud_creds" {
-    description = "Access key and Secret key for Redis Cloud account"
+
+#### Variables used in modules
+##################### AWS Variables
+
+#### Provider variables
+variable "aws_creds" {
+    description = "Access key and Secret key for AWS [Access Keys, Secret Key]"
 }
 
-variable "rediscloud_subscription_name" {
-    description = "Name of RedisCloud subscription"
+variable "prefix_name" {
+    description = "prefix to AWS resources"
+    default = "redisuser"
+}
+
+#### Declare the list of subnet CIDR blocks
+variable "subnet_cidr_block" {
+    description = "subnet_cidr_block"
+    default = "10.0.1.0/24"
 }
 
 ##### Subscription Peering
-
-variable "rediscloud_networking_deployment_cidr" {
-    description = "the CIDR of your RedisCloud deployment"
-}
-
-variable "aws_customer_application_vpc_region" {
-    description = "aws_customer_application_vpc_region"
-}
 
 variable "aws_customer_application_aws_account_id" {
     description = "aws_customer_application_aws_account_id"
 }
 
-variable "aws_customer_application_vpc_id" {
-    description = "aws_customer_application_vpc_id"
-}
-
 variable "aws_customer_application_vpc_cidr" {
     description = "aws_customer_application_vpc_cidr"
+    default = "10.0.0.0/16"
 }
 
-### AWS TERRAFORM PROVIDER
+##################### Redis Cloud Variables
 
-variable "aws_creds" {
-    description = "Access key and Secret key for AWS [Access Keys, Secret Key]"
+variable "rediscloud_creds" {
+    description = "Access key and Secret key for Redis Cloud account"
 }
 
-variable "aws_vpc_route_table_id" {
-    description = "AWS VPC Route Table Id"
+variable "cc_type" {
+    description = "credit card type"
+    default = "Visa"
+}
+
+variable "cc_last_4" {
+    description = "Last 4 digits for payment method"
+}
+
+##### Redis Cloud Subscription Variables
+
+variable "rc_networking_deployment_cidr" {
+    description = "the CIDR of your RedisCloud deployment"
+    default = "10.1.0.0/24"
 }
